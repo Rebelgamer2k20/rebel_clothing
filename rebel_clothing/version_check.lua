30db2b9cf759992840e53cee45e53fbbcc9a7cd0
Rebelgamer2k20 <107582531+Rebelgamer2k20@users.noreply.github.com>
2025-05-20 01:41:29 -0400

    Update version_check.lua

--- rebel_clothing/version_check.lua
+++ rebel_clothing/version_check.lua
@@ -1,7 +1,7 @@
 -- version_check.lua
 
 local CurrentVersion = "1.0.0.0"
-local VersionURL = "https://raw.githubusercontent.com/Rebelgamer2k20/rebel_clothing/refs/heads/main/rebel_clothing/version.json?token=GHSAT0AAAAAADDHTVFRS35U7S4VH5XS5QYI2BMCKSA" -- ⬅️ Change this to your actual URL
+local VersionURL = "https://raw.githubusercontent.com/Rebelgamer2k20/rebel_clothing/refs/heads/main/rebel_clothing/version_check.lua?token=GHSAT0AAAAAADDHTVFRLUBWTP3C5ZQHPGK42BMCY2A" -- ⬅️ Change this to your actual URL
 
 local function splitVersion(version)
     local parts = {}
