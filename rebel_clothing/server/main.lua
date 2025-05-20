79dde61723042afa7236d84a5ac59d923c6b7a64
Rebelgamer2k20 <107582531+Rebelgamer2k20@users.noreply.github.com>
2025-05-20 03:03:14 -0400

    changed location of ascii

--- rebel_clothing/server/main.lua
+++ rebel_clothing/server/main.lua
@@ -1,16 +1,4 @@
--- 🌟 Mechanic Shop ASCII Logo 🌟
-local creatorLogo = [[
- ____      _          _  ____                          ____  _    ____   ___  
-|  _ \ ___| |__   ___| |/ ___| __ _ _ __ ___   ___ _ _|___ \| | _|___ \ / _ \ 
-| |_) / _ \ '_ \ / _ \ | |  _ / _` | '_ ` _ \ / _ \ '__|__) | |/ / __) | | | |
-|  _ <  __/ |_) |  __/ | |_| | (_| | | | | | |  __/ |  / __/|   < / __/| |_| |
-|_| \_\___|_.__/ \___|_|\____|\__,_|_| |_| |_|\___|_| |_____|_|\_\_____|\___/ 
-]]
 
-local resourceVersion = "1.0.0.3"
-
-print(creatorLogo)
-print("^3[Rebel_Clothing]^0 Resource Version: ^2" .. resourceVersion .. "^0")
 
 local QBCore = exports['qb-core']:GetCoreObject()
 
