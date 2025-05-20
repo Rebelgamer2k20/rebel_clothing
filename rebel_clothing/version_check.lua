768812518f2dd0f6293e763484abca0b4be2c375
Rebelgamer2k20 <107582531+Rebelgamer2k20@users.noreply.github.com>
2025-05-20 02:54:34 -0400

    updated version

--- rebel_clothing/version_check.lua
+++ rebel_clothing/version_check.lua
@@ -1,6 +1,6 @@
 -- version_check.lua-------
 
-local CurrentVersion = "1.0.0.3"
+local CurrentVersion = "1.0.0.4"
 local VersionURL = "https://raw.githubusercontent.com/Rebelgamer2k20/rebel_clothing/refs/heads/main/rebel_clothing/version_check.json" -- <-- Use the correct URL
 
 local function splitVersion(version)
