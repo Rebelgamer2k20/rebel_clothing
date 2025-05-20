450f6111e51d56daead5c11db7c5486558a54558
Rebelgamer2k20 <107582531+Rebelgamer2k20@users.noreply.github.com>
2025-05-20 02:13:16 -0400

    Update main.lua

--- rebel_clothing/server/main.lua
+++ rebel_clothing/server/main.lua
@@ -1,3 +1,17 @@
+-- 🌟 Mechanic Shop ASCII Logo 🌟
+local creatorLogo = [[
+ ____      _          _  ____                          ____  _    ____   ___  
+|  _ \ ___| |__   ___| |/ ___| __ _ _ __ ___   ___ _ _|___ \| | _|___ \ / _ \ 
+| |_) / _ \ '_ \ / _ \ | |  _ / _` | '_ ` _ \ / _ \ '__|__) | |/ / __) | | | |
+|  _ <  __/ |_) |  __/ | |_| | (_| | | | | | |  __/ |  / __/|   < / __/| |_| |
+|_| \_\___|_.__/ \___|_|\____|\__,_|_| |_| |_|\___|_| |_____|_|\_\_____|\___/ 
+]]
+
+local resourceVersion = "1.0.0.0"
+
+print(creatorLogo)
+print("^3[Mechanic Shop]^0 Resource Version: ^2" .. resourceVersion .. "^0")
+
 local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterServerEvent("clothingstore:chargePlayer", function()
