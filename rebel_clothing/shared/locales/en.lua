50e3f8e67170f4ebf32a4e4a1fb71d993552afa5
Rebelgamer2k20 <107582531+Rebelgamer2k20@users.noreply.github.com>
2025-05-20 01:14:54 -0400

    local lua fix
    added french

--- rebel_clothing/shared/locales/en.lua
+++ rebel_clothing/shared/locales/en.lua
@@ -1,14 +1,20 @@
-Locales = Locales or {}
+Locales = {}
 
 Locales['en'] = {
-    menu_title = "👕 Clothing Options",
-    try_clothes = "🧥 Try on Clothes",
-    try_clothes_desc = "Use appearance editor",
-    finish_pay = "💾 Finish & Pay",
-    finish_pay_desc = "Save outfit & checkout",
-    paid_success = "You paid $%d for your outfit!",
-    paid_error = "Not enough cash!"
+    ['menu_title'] = "👕 Clothing Options",
+    ['try_clothes'] = "🧥 Try on Clothes",
+    ['try_clothes_desc'] = "Use appearance editor",
+    ['finish_pay'] = "💾 Finish & Pay",
+    ['finish_pay_desc'] = "Save outfit & checkout",
+    ['paid_success'] = "You paid $%d for your outfit!",
+    ['paid_error'] = "Not enough cash!"
 }
---[[
-
-    Rebel Clothing Store
+Locales['fr'] = {
+    ['menu_title'] = "👕 Options de Vêtements",
+    ['try_clothes'] = "🧥 Essayer des Vêtements",
+    ['try_clothes_desc'] = "Utiliser l'éditeur d'apparence",
+    ['finish_pay'] = "💾 Terminer & Payer",
+    ['finish_pay_desc'] = "Enregistrer la tenue et passer à la caisse",
+    ['paid_success'] = "Vous avez payé $%d pour votre tenue !",
+    ['paid_error'] = "Pas assez d'argent !"
+}
